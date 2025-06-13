5c7b822a843c248df374ff74d86ed5d156a50a38
YS1210000 <94222509+YS1210000@users.noreply.github.com>
2025-06-13 15:00:25 +0900

    Create 9252.py

---
+++ baekjoon/9252.py
@@ -0,0 +1,19 @@
+import sys
+
+strA = " "+sys.stdin.readline().rstrip()
+strB = " "+sys.stdin.readline().rstrip()
+
+table = [[[] for _ in range(len(strA))] for _ in range(len(strB))]
+for i in range(1, len(strA)):
+    for j in range(1, len(strB)):
+        if strA[i] == strB[j]:
+            table[j][i] += table[j-1][i-1] + [strA[i]]
+        else:
+            if len(table[j-1][i]) >= len(table[j][i-1]):
+                table[j][i] = table[j-1][i]
+            else:
+                table[j][i] = table[j][i-1]
+
+print(len(table[-1][-1]))
+if len(table[-1][-1]) != 0:
+    print("".join(table[-1][-1]))
